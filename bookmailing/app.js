a2cb5ced060dc6eb3a17abe91aa57df96c7ae31e
Eunbin Kwon <kwoneunbini@gmail.com>
2024-09-19 19:47:31 +0900

    chore: added psuedo code description

--- bookmailing/app.js
+++ bookmailing/app.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 8080
+// NPM
+import express from 'express'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// App Settings
+let app = express()
+let port = 3000
+
+app.post('/subscribe', (req, res) => {
+        // Check if the email exists first of all
+        
+        // If it doesn't..
+            // Then validate the email
+                // And add it to the database
+                // Otherwise show errors
+        // Or a real error if something really goes wrong
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/unsubscribe', (req, res) => {
+    // Unsubscribe email
+        // When we unsubscribe, check for an email
+            // If it exists, remove it
+            // Otherwise the user wasn't even subscribed to begin with
 })
+
+app.listen(port)
